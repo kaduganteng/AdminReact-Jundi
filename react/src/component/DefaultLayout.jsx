c55b984f177f9231bd0d920333b3c58d0ed15617
kaduganteng <rayhanjundi77@gmail.com>
2023-02-18 23:07:46 +0700

    token access

--- react/src/component/DefaultLayout.jsx
+++ react/src/component/DefaultLayout.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { userStateContext } from "../contexts/ContextProvider";
 
-export default function DefaultLayout(){
-    return(
+export default function DefaultLayout() {
+    const { user, token } = userStateContext();
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
+    return (
         <div>
-        <Outlet/>
+            <Outlet />
         </div>
-    )
+    );
 }
--- react/src/component/GuestLayout.jsx
+++ react/src/component/GuestLayout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { userStateContext } from "../contexts/ContextProvider";
 
-export default function GuestLayout(){
-    return(
+export default function GuestLayout() {
+    const { token } = userStateContext();
+    if (token) {
+        return <Navigate to="/" />;
+    }
+    return (
         <div>
-        <div>
-            This is only for guest 
-            <Outlet/>
+            <div>
+                This is only for guest
+                <Outlet />
+            </div>
         </div>
-        </div>
-    )
+    );
 }
